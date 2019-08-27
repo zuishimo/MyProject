20e06e734adff58f3b8fd74c3897c59f8a0753e0
Lion <2766159199@qq.com>
2019-08-27 13:33:23 +0800

    test

--- src/List/TestHashMap.java
+++ src/List/TestHashMap.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class TestHashMap {
-
+//	System.out.println("Ð¡ÐÞ¸Ä");
 	public static void main(String[] args) {
 		
 		int total = 3000000;
